cad235411f375602c7d2cc9b55652fbcbfeac1ee
johnsonafool <88415078+johnsonafool@users.noreply.github.com>
2022-10-15 20:49:31 +0800

    add pretiter and eslint config

--- components/Title.tsx
+++ components/Title.tsx
@@ -1,9 +1,12 @@
+import { useState } from "react";
 import Hey from "./subtitle";
 
 export default function Title() {
+  const [title, setTile] = useState<String>("Hello");
   return (
     <h1>
-      Hello <Hey></Hey>
+      {title}
+      <Hey></Hey>
     </h1>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,5 +3,14 @@ import styles from "../styles/Home.module.css";
 import nextPackage from "package.json";
 
 export default function Home({}) {
-  return <h1 className="text-3xl font-bold underline">hello world!</h1>;
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">hello world!</h1>
+      <h1>hello world!</h1>
+      <h3 className="">my name is john doe</h3>
+      <button className="text-white px-4 sm:px-8 py-2 sm:py-3 bg-sky-700 hover:bg-sky-800">
+        ...
+      </button>
+    </>
+  );
 }
