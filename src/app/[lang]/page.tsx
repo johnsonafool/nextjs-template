d6d495615a9154f5fdd3769c1e4c5835f5877bbe
johnsonafool <johnsonafool@gmail.com>
2023-09-25 11:00:48 +0800

    feat: setup i18n with nextjs

---
+++ src/app/[lang]/page.tsx
@@ -0,0 +1,27 @@
+import { getDictionary } from "../../get-dictionary";
+import { Locale } from "../../i18n-config";
+import Counter from "./components/counter";
+import LocaleSwitcher from "./components/locale-switcher";
+import { ModeToggle } from "@/components/theme-toggle";
+
+export default async function Home({
+  params: { lang },
+}: {
+  params: { lang: Locale };
+}) {
+  const dictionary = await getDictionary(lang);
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <LocaleSwitcher />
+      <p>Current locale: {lang}</p>
+      <p>
+        This text is rendered on the server:{" "}
+        {dictionary["server-component"].welcome}
+      </p>
+      <Counter dictionary={dictionary.counter} />
+      <ModeToggle />
+      <p>nextjs app</p>
+    </main>
+  );
+}
